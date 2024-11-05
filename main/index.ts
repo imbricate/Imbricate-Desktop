34816e710abb67eee9d259e3c93cd1d425edbfa0
wmxpy <wm@mengw.io>
2024-11-05 10:18:32 -0600

    add dev workflow

--- main/index.ts
+++ main/index.ts
@@ -1,1 +1,7 @@
-import { app, BrowserWindow } from "electron/main";
+import { DatabaseView } from "./view/database";
+import { app } from "electron";
+
+app.on("ready", () => {
+    console.log("App is ready");
+    DatabaseView.showView();
+});
--- main/view/database.ts
+++ main/view/database.ts
@@ -1,16 +1,23 @@
-import { BrowserWindow } from "electron"
+import { BrowserWindow } from "electron";
 
 export class DatabaseView {
 
-    private static windowStack: Map<string, BrowserWindow> = new Map<string, BrowserWindow>()
+    private static windowStack: Map<string, BrowserWindow> = new Map<string, BrowserWindow>();
 
     public static showView(): void {
-        const window = new BrowserWindow({
-            width: 800,
-            height: 600,
-            webPreferences: {
-                preload: "http://localhost:3000/preload.js"
-            }
-        })
+        const window = this.getOrCreateWindow("database");
+        window.loadURL("http://localhost:5711");
+    }
+
+    private static getOrCreateWindow(name: string): BrowserWindow {
+        let window = this.windowStack.get(name);
+        if (!window) {
+            window = new BrowserWindow({
+                width: 800,
+                height: 600,
+            });
+            this.windowStack.set(name, window);
+        }
+        return window;
     }
 }
